99a5ca290d6bfe3d5ee9fd6104635a1ba3785852
Adriano Del Vigna <adriano.delvigna@gmail.com>
2018-12-22 16:39:35 -0200

    Creates an AppCompat Toolbar/App bar.

--- app/src/main/java/udacity/androidnanodegree/adriano/capstone/MainActivity.java
+++ app/src/main/java/udacity/androidnanodegree/adriano/capstone/MainActivity.java
@@ -2,7 +2,12 @@ package udacity.androidnanodegree.adriano.capstone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import udacity.androidnanodegree.adriano.capstone.fragments.constructorstandings.ConstructorFragment;
 import udacity.androidnanodegree.adriano.capstone.fragments.constructorstandings.models.ConstructorStanding;
 import udacity.androidnanodegree.adriano.capstone.fragments.driverstandings.DriverFragment;
@@ -15,10 +20,30 @@ public class MainActivity extends AppCompatActivity implements
         DriverFragment.OnListFragmentInteractionListener,
         ConstructorFragment.OnListFragmentInteractionListener {
 
+    @BindView(R.id.toolbar) Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_about:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
