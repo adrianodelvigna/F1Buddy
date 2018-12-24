f5a19bbfd1238c78ebb2ed3941d77877cb74ee3d
Adriano Del Vigna <adriano.delvigna@gmail.com>
2018-12-24 16:46:08 -0200

    Creates the service that will update the widget countdown.
    This service will be called from an alarm that is set to repeat at
    regular intervals (every 1 minute or so).
    
    The service will reach to widget's remote views and update the
    countdown to the next race accordingly.

---
+++ app/src/main/java/udacity/androidnanodegree/adriano/capstone/widget/NextRaceCountdownService.java
@@ -0,0 +1,16 @@
+package udacity.androidnanodegree.adriano.capstone.widget;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class NextRaceCountdownService extends Service {
+    public NextRaceCountdownService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
