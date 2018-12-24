23cb72bcfe44065b2526f828aefbac4f77535820
Adriano Del Vigna <adriano.delvigna@gmail.com>
2018-12-24 16:48:20 -0200

    Prevents the service from being bound to.

--- app/src/main/java/udacity/androidnanodegree/adriano/capstone/widget/NextRaceCountdownService.java
+++ app/src/main/java/udacity/androidnanodegree/adriano/capstone/widget/NextRaceCountdownService.java
@@ -10,7 +10,6 @@ public class NextRaceCountdownService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
     }
 }
